7c3cb0a7ac4e8423326cca0d14ae91c565c9129a
Lendra <lendrjhh@gmail.com>
2024-11-06 14:09:32 +0700

    Add files via upload

---
+++ web/register.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <?php include "layout/header.html"?>
+    <h3>DAFTAR AKUN</h3>
+    <form>
+        <input type="text" placeholder="username" name="username"/><br>
+        <input type="text" placeholder="password" name="password"/><br>
+        <button type="submit">daftar</button>
+    </form>
+    <?php include "layout/footer.html" ?>
+</body>
+</html>
